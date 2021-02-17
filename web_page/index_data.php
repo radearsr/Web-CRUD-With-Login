b8befb62f9b8ad7ea82aca85a1a472d5a534cdc4
radearsr <radea.surya1@gmail.com>
2021-02-17 20:35:32 +0700

    Menambahkan Menu Pada Tampilan Bar

--- web_page/index_data.php
+++ web_page/index_data.php
@@ -46,15 +46,24 @@ session_start();
               <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
                 <div class="menu_section">
                   <ul class="nav side-menu">
-                    <li><a href="index_data.php?page=tmp_utama"><i class="fa fa-home"></i>Tampilan Data<span class="fa fa-chevron"></span></a>
+                    <!-- Bar Menu Pertama -->
+                    <li>
+                    <a href="index_data.php?page=tmp_utama"><i class="fa fa-home"></i>Tampilan Data<span class="fa fa-chevron"></span></a>
                     </li>
-                    
+                    <a href="index_data.php?page=tmp_utama"><i class="fa fa-home"></i>Tampilan Data<span class="fa fa-chevron"></span></a>
+                    </li>
+                    <a href="index_data.php?page=tmp_utama"><i class="fa fa-home"></i>Tampilan Data<span class="fa fa-chevron"></span></a>
+                    </li>
+                    <a href="index_data.php?page=tmp_utama"><i class="fa fa-home"></i>Tampilan Data<span class="fa fa-chevron"></span></a>
+                    </li>                    
+                    <!-- Bar Menu Kedua -->
                     <li><a href="#"><i class="fa fa-table"></i> Management Data <span class="fa fa-chevron-down"></span></a>
                       <ul class="nav child_menu">
                         <li><a href="index_data.php?page=tambah_data">Tambah Data</a></li>
                         <li><a href="index_data.php?page=tmp_revisi">Revisi Data</a></li>
                       </ul>
                     </li>
+
                   </ul>
                 </div>
               </div>
