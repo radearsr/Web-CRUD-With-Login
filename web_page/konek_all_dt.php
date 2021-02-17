d973be1a2518992e151aa4cb7be30b2237ae9a8d
radearsr <radea.surya1@gmail.com>
2021-02-17 20:36:29 +0700

    Mengganti Koneksi Database Menjadi LocalHost

--- web_page/konek_all_dt.php
+++ web_page/konek_all_dt.php
@@ -2,10 +2,10 @@
 //koneksi ke database mysql,
 
 // db Local Xampp
-// $koneksi = mysqli_connect("localhost", "root", "", "mydata");
+$koneksi = mysqli_connect("localhost", "root", "", "mydata");
 
 // db Hosting
-$koneksi = mysqli_connect("sql202.epizy.com", "epiz_27905230", "UQy0sos4tv", "epiz_27905230_mydatabase");
+// $koneksi = mysqli_connect("sql202.epizy.com", "epiz_27905230", "UQy0sos4tv", "epiz_27905230_mydatabase");
 
 //cek jika koneksi ke mysql gagal, maka akan tampil pesan berikut
 if (mysqli_connect_errno()){
