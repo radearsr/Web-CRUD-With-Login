4f0996726176d1d1974fec850ae4f3fec92e5093
radearsr <radea.surya1@gmail.com>
2021-02-22 06:09:31 +0700

    Membuat File Untuk Menyederhanakan config

---
+++ web_page/config_add_data.php
@@ -0,0 +1,9 @@
+<?php 
+
+// db Local Xampp
+$connect = mysqli_connect("localhost", "root", "", "mydata");
+
+// db Hosting
+// $connect = mysqli_connect("sql202.epizy.com", "epiz_27905230", "UQy0sos4tv", "epiz_27905230_mydatabase");
+
+ ?>
